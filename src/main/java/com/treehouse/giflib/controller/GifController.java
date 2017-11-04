6b97456573a117ef003b8501c04dce8df605c5ee
Varun Upadhyay <varun.u28@gmail.com>
2017-11-04 07:38:38 -0700

    Added the Gif repository and connected the controller to it

--- src/main/java/com/treehouse/giflib/controller/GifController.java
+++ src/main/java/com/treehouse/giflib/controller/GifController.java
@@ -1,13 +1,28 @@
 package com.treehouse.giflib.controller;
 
+import com.treehouse.giflib.data.GifRepository;
+import com.treehouse.giflib.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class GifController {
 
+    @Autowired
+    private GifRepository gifRepository;
+
     @RequestMapping("/")
     public String listGifs() {
         return "home";
+    }
+
+    @RequestMapping("/gif")
+    public String gifDetails(ModelMap modelMap) {
+        Gif gif = gifRepository.findByName("android-explosion");
+        modelMap.put("gif", gif);
+        return "gif-details";
     }
 }
