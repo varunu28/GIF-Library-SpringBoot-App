b6a678badeb2788d0f6f9c2b9106525004037e69
Varun Upadhyay <varun.u28@gmail.com>
2017-11-03 21:58:34 -0700

    First commit.Added controllers and Views

---
+++ src/main/java/com/treehouse/giflib/controller/GifController.java
@@ -0,0 +1,13 @@
+package com.treehouse.giflib.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GifController {
+
+    @RequestMapping("/")
+    public String listGifs() {
+        return "home";
+    }
+}
