7b87bd0a338bf9175d2b1529090f7130541f109b
Varun Upadhyay <varun.u28@gmail.com>
2017-11-04 17:50:05 -0700

    Created the data model and added the data repository

--- src/main/java/com/treehouse/giflib/controller/GifController.java
+++ src/main/java/com/treehouse/giflib/controller/GifController.java
@@ -5,7 +5,10 @@ import com.treehouse.giflib.model.Gif;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 
 @Controller
@@ -15,13 +18,15 @@ public class GifController {
     private GifRepository gifRepository;
 
     @RequestMapping("/")
-    public String listGifs() {
+    public String listGifs(ModelMap modelMap) {
+        List<Gif> allGifs = gifRepository.getAllGifs();
+        modelMap.put("gifs", allGifs);
         return "home";
     }
 
-    @RequestMapping("/gif")
-    public String gifDetails(ModelMap modelMap) {
-        Gif gif = gifRepository.findByName("android-explosion");
+    @RequestMapping("/gif/{name}")
+    public String gifDetails(@PathVariable String name, ModelMap modelMap) {
+        Gif gif = gifRepository.findByName(name);
         modelMap.put("gif", gif);
         return "gif-details";
     }
