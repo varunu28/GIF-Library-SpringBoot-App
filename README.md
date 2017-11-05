1a0f63de82b6f51d35b6092aa36b388b25c28c73
Varun Upadhyay <varun.u28@gmail.com>
2017-11-05 06:10:41 -0800

    Added the functionality for categories and removed files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # GIF-Library-SpringBoot-App
 A GIF library webapp done using Spring Boot and Thymeleaf
+
+#### To-Do
+
+ - Implementing the Favorites Page (/favorites uri)
+ - Implementing the Search Page (Through Search Bar)
---
+++ src/main/java/com/treehouse/giflib/controller/CategoryController.java
@@ -0,0 +1,41 @@
+package com.treehouse.giflib.controller;
+
+import com.treehouse.giflib.data.CategoryRepository;
+import com.treehouse.giflib.data.GifRepository;
+import com.treehouse.giflib.model.Category;
+import com.treehouse.giflib.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class CategoryController {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private GifRepository gifRepository;
+
+    @RequestMapping("/categories")
+    public String listCategories(ModelMap modelMap) {
+        List<Category> allCategories = categoryRepository.getAllCategories();
+        modelMap.put("categories", allCategories);
+        return "categories";
+    }
+
+    @RequestMapping("/category/{id}")
+    public String category(@PathVariable int id, ModelMap modelMap) {
+        Category category = categoryRepository.findById(id);
+        modelMap.put("category", category);
+
+        List<Gif> gifs = gifRepository.findByCategoryId(id);
+        modelMap.put("gifs", gifs);
+
+        return "category";
+    }
+}
